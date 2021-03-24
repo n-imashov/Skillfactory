af47e595684fe86dabe9f27ecc8f050c02ffa073
n-imashov <80469886+n-imashov@users.noreply.github.com>
2021-03-24 20:02:28 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_sex(self):
+        return self.sex
+
+    def get_age(self):
+        return self.age
