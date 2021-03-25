f4ca765b02e3f6875f8cd3e0b8f011090c52e619
n-imashov <80469886+n-imashov@users.noreply.github.com>
2021-03-25 17:11:07 +0600

    Delete testPETS.py

--- testPETS.py
+++
@@ -1,8 +0,0 @@
-from main import Cat
-
-obj_1 = Cat("Куман,", "пасанчик,", 1.5)
-obj_2 = Cat("Травоман,", "пасанчик,", 2.5)
-obj_3 = Cat("Анмани,", "деффчонка,", 3)
-print(obj_1.get_name(), obj_1.get_sex(), obj_1.get_age())
-print(obj_2.get_name(), obj_2.get_sex(), obj_2.get_age())
-print(obj_3.get_name(), obj_3.get_sex(), obj_3.get_age())
