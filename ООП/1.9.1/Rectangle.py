22eaf6468eb02d2e27c8854e1c8d732978240711
n-imashov <80469886+n-imashov@users.noreply.github.com>
2021-03-25 17:09:44 +0600

    Add files via upload

---
+++ ООП/1.9.1/Rectangle.py
@@ -0,0 +1,26 @@
+from main import Rectangle, Square, Circle
+
+# далее создаем два прямоугольника
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(12, 5)
+# вывод площади наших двух прямоугольников
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+print(square_1.get_area_square(),
+      square_2.get_area_square())
+
+circle_1 = Circle(5)
+circle_2 = Circle(10)
+
+figures = [rect_1, rect_2, square_1, square_2, circle_1, circle_2]
+
+for figure in figures:
+    if isinstance(figure, Square):
+        print(figure.get_area_square())
+    elif isinstance(figure, Circle):
+        print(figure.get_circle_area())
+    else:
+        print(figure.get_area())
