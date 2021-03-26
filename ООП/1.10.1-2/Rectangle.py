3afbc0f73fd287d8991be06a50b22855ebf4cd49
n-imashov <80469886+n-imashov@users.noreply.github.com>
2021-03-26 11:56:45 +0600

    Update Rectangle.py

--- ООП/1.10.1-2/Rectangle.py
+++ ООП/1.10.1-2/Rectangle.py
@@ -1,13 +1,13 @@
 # 1.10.1
 class Rectangle:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, w, h):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.width = w
+        self.height = h
 
     def __str__(self):
-        return f'x: {self.x} \ny: {self.y} \nwidth: {self.width} \nheight: {self.height}'
+        return f'x: {self.x} \ny: {self.y} \nw: {self.width} \nh: {self.height}'
 
 
 figure = Rectangle(5, 10, 50, 100)
@@ -16,12 +16,12 @@ print(figure)
 
 # 1.10.2
 class Rectangle2D:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
 
     def get_area(self):
-        return f"Прямоугольник {self.width} {self.height} {self.width * self.height}"
+        return f"Прямоугольник (длина, высота, площадь): {self.width}, {self.height}, {self.width * self.height}"
 
 
 figure = Rectangle2D = Rectangle2D(5, 10)
